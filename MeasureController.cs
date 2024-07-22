d3e106e4caca37c396cee6ec1376adc85f1cbb3c
Anton <anton13395@gmail.com>
2024-07-22 10:45:18 +0300

    v1.0001

---
+++ MeasureController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+//[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+//[assembly: Guid("47f27397-f9ca-4ccc-b39a-80749aeccd1a")]
+[Route("api/[controller]")]
+[ApiController]
+public class ItemsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ItemsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/items
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MeasureValue>>> GetItems()
+    {
+        return await _context.Items.ToListAsync();
+    }
+
+    // POST: api/items
+    [HttpPost]
+    public async Task<ActionResult<MeasureValue>> PostItem(MeasureValue item)
+    {
+        _context.Items.Add(item);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+    }
+
+    // GET: api/items/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<MeasureValue>> GetItem(int id)
+    {
+        var item = await _context.Items.FindAsync(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return item;
+    }
+}
